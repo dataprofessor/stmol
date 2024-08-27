36b5744f2c79d5241d4ea82af78cfcd260f52194
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-08-27 11:40:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
-import streamlit as st
+from stmol import showmol
+import py3Dmol
 
-st.title('🎈 App Name')
+st.title('🎈 stmol')
 
-st.write('Hello world!')
+# Structure of thrombin inhibited by AERUGINOSIN298-A from a BLUE-GREEN ALGA
+xyzview = py3Dmol.view(query='pdb:1A2C') 
+xyzview.setStyle({'cartoon':{'color':'spectrum'}})
+showmol(xyzview, height = 500,width=800)
