7b2c496f1994248ac9a95c0e374ec692abcf3ac1
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-08-28 08:48:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from stmol import showmol
 import py3Dmol
 
+st.title('🎈 stmol')
+
 # Structure of Green Fluorescent Protein from Aequoria victoria
 xyzview = py3Dmol.view(query='pdb:1EMA') 
 xyzview.setStyle({'cartoon':{'color':'spectrum'}})
